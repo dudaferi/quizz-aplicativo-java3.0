957fcef01e18d761acc27f6eaa23c212478edaf8
dudaferi <141652124+dudaferi@users.noreply.github.com>
2023-11-13 12:10:10 -0300

    Update question.js

--- js/question.js
+++ js/question.js
@@ -52,9 +52,91 @@ let questions = [
       "Saturno e Júpiter",
       "Vênus e Urano",
       "Terra e Marte",
-      "Mercúrio e Saturno"
+      "Mercúrio e Saturno" 
     ]
   },
+  {
+    numb: 6, 
+    question:"Qual a capital da Bahia?" 
+    answer: "Salvador" 
+options:[  
+  "Maceió" 
+  "Fortaleza" 
+"São Luís" 
+"Recife"
+] 
+}, 
+{ 
+  numb: 7,  
+    question: "Qual o planeta mais próximo do Sol?" 
+  answer:"Mercúrio" 
+  options:[ 
+    "Netuno"
+   "Júpiter"
+   "Mercúrio"
+   "Terra" 
+  ] 
+}, 
+{ 
+  numb: 8, 
+    question: "Quantos continentes existem?" 
+  answer: "6" 
+  options:[ 
+     "2"
+    "8"
+    "4"
+    "6" 
+  ] 
+}, 
+{ 
+  numb: 10,
+  question: " Quais as duas línguas mais faladas no mundo?" 
+    answer: "Inglês e mandarim chinês" 
+  options:[ 
+    "Inglês e espanhol"
+ "Inglês e mandarim chinês"
+ "Mandarim chinês e francês"
+ "Inglês e português" 
+] 
+}, 
+{ 
+  numb: 11,
+  question: "Qual a maior floresta tropical do mundo?" 
+  answer: "Floresta Amazônica"
+  options:[ 
+     "Mata Atlântica"
+"Pampas"
+ "Pantanal"
+"Floresta Amazônica" 
+  ] 
+}, 
+{  
+  numb: 12, 
+  question: "Quem pintou Mona Lisa?" 
+  answer:"Leonardo da Vinci."
+  options: [ 
+ "Leonardo da Vinci"
+ "Salvador Dalí"
+ "Van Gogh"
+ "Tarsila do Amaral" 
+  ] 
+}, 
+{ 
+    numb: 13, 
+  question: "Qual a personagem mais famosa de Maurício de Sousa?" 
+  answer:"Mônica"
+  options: [
+  "Mafalda"
+ "Mônica"
+ "Smurfette"
+"Magali" 
+  ] 
+}, 
+{  
+  numb: 14, 
+  question: " Qual o nome popular do cloreto de sódio?" 
+  answer:"Mônica"
+  options: [
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
